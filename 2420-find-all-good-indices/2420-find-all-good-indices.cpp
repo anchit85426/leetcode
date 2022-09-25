6e8e8f19b82299052e145e574874b3e41ee0b315
anchit jain <75929617+anchit10@users.noreply.github.com>
2022-09-25 13:11:15 +0530

    Time: 376 ms (25.00%), Space: 88.8 MB (25.00%) - LeetHub

--- 2420-find-all-good-indices/2420-find-all-good-indices.cpp
+++ 2420-find-all-good-indices/2420-find-all-good-indices.cpp
@@ -15,10 +15,7 @@ public:
                 suffix[i]+=suffix[i+1];
             }
         }
-        for(auto it :suffix){
-            cout<<it<<" ";
-        }
-        cout<<endl;
+
         for(int i=k;i<n-k;i++){
             if(min(prefix[i-1],suffix[i+1])>=k){
                 ans.push_back(i);
