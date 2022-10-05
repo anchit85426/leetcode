66b12b5b3ecff2d5f4c2b49c7960e59409c80907
anchit jain <75929617+anchit10@users.noreply.github.com>
2022-10-05 10:00:23 +0530

    Time: 478 ms (40.64%), Space: 9.5 MB (99.21%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
@@ -1,17 +1,24 @@
 class Solution {
     private:
-        bool SubsetSum(int idx,vector<int>&nums,int target,vector<vector<int>>&dp){
-            if(idx==0) return nums[0]==target;
-            if(target==0) return true;
-            if(dp[idx][target]!=-1) return dp[idx][target];
-            bool nottake=SubsetSum(idx-1,nums,target,dp);
-            bool take=false;
-            if(target>=nums[idx]) take=SubsetSum(idx-1,nums,target-nums[idx],dp);
-            return dp[idx][target]=take||nottake;
+        bool SubsetSum2(int idx,vector<int>&nums,int target){
+            vector<bool>prev(target+1,false),curr(target+1,false);
+            prev[0]=curr[0]=true;
+            if(nums[0]<=target)prev[nums[0]]=true;
+            for(int i=1;i<idx;i++){
+                for(int k=1;k<=target;k++){
+                    bool nottake=prev[k];
+                    bool take=false;
+                    if(nums[i]<=k) take=prev[k-nums[i]];
+                    curr[k]=take|nottake;
+                }
+                prev=curr;
+            }
+            return  prev[target];
         }
 public:
     bool canPartition(vector<int>& nums) {
         int sum=0;
+        if(nums.size()==1) return false;
         for(auto it :nums){
             sum+=it;
         }
@@ -19,7 +26,7 @@ public:
             return false;
         }
         int target=sum/2;
-        vector<vector<int>>dp(201,vector<int>(20000,-1));
-        return SubsetSum(nums.size()-1,nums,target,dp);
+
+        return SubsetSum2(nums.size(),nums,target);
     }
 };
