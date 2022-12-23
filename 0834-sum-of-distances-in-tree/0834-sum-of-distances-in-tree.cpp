1565225943b65c939f2c587a520b53190eab592a
anchit jain <75929617+anchit10@users.noreply.github.com>
2022-12-23 22:40:17 +0530

    Time: 560 ms (42.42%), Space: 131.3 MB (6.76%) - LeetHub

--- 0834-sum-of-distances-in-tree/0834-sum-of-distances-in-tree.cpp
+++ 0834-sum-of-distances-in-tree/0834-sum-of-distances-in-tree.cpp
@@ -1,38 +1,40 @@
 class Solution {
     vector<unordered_set<int>>adj;
+    vector<int>count;
+    vector<int>res;
     public :
-        void dfs(int root,int parent,vector<int>&count,vector<int>&res){
+        void dfs(int root,int parent){
         for(auto it :adj[root]){
             if(it==parent){
                 continue;
             }
-            dfs(it,root,count,res);
+            dfs(it,root);
             count[root]+=count[it];
             res[root]+=res[it]+count[it];
             
         }
     }
-    void dfs2(int root,int parent,vector<int>&count,vector<int>&res,int n){
+    void dfs2(int root,int parent){
         for(auto it :adj[root]){
             if(it==parent){
                 continue;
             }
            
-            res[it]=res[root]-count[it]+(n-count[it]);
-             dfs2(it,root,count,res,n);
+            res[it]=res[root]-count[it]+(count.size()-count[it]);
+             dfs2(it,root);
         }
     }
 public:
     vector<int> sumOfDistancesInTree(int n, vector<vector<int>>& edges) {
-        vector<int>res(n,0);
-        vector<int>count(n,1);
+        res.resize(n,0);
+        count.resize(n,1);
         adj.resize(n);
         for(auto it:edges){
             adj[it[0]].insert(it[1]);
             adj[it[1]].insert(it[0]);
         }
-        dfs(0,-1,count,res);
-        dfs2(0,-1,count,res, n);
+        dfs(0,-1);
+        dfs2(0,-1);
         return res;
         
     }
