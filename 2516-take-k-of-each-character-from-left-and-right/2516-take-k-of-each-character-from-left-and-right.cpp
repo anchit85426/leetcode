374a54ed80bf33222d816721fbaab6847e227e9e
anchit jain <75929617+anchit10@users.noreply.github.com>
2022-12-25 17:25:32 +0530

    Time: 2542 ms (8.33%), Space: 426 MB (33.33%) - LeetHub

--- 2516-take-k-of-each-character-from-left-and-right/2516-take-k-of-each-character-from-left-and-right.cpp
+++ 2516-take-k-of-each-character-from-left-and-right/2516-take-k-of-each-character-from-left-and-right.cpp
@@ -1,29 +1,52 @@
 class Solution {
-public:
-    int takeCharacters(string s, int k) {
-        
-       int N = s.length(),j = 0, ans = N+1,window = 0;
-       unordered_map<char,int>count;
-
-       for(auto c : s)
-            count[c]++;
-
-       if(count['a'] < k || count['b'] <k || count['c']<k)  
-            return -1;
-
-       for(int i=0;i<N;i++){
-
-          count[s[i]]--;
-          window++;
-
-            while(count[s[i]]<k){
-                count[s[j++]]++;
-                window--;
-            }
-          
-          ans = min(ans,N- window);
-       }
-    
-        return ans;
+    bool isSatisfied(vector<int>v,int k){
+        if(v[0]>=k and v[1]>=k and v[2]>=k){
+            return true;
+        }
+        return false;
     }
+private:
+    bool isValid(string s,int k,int cnt){
+        vector<int>v(3,0);
+        int n=s.length();
+        for(int i=0;i<cnt;i++){
+            v[s[i]-'a']++;
+        }
+        if (isSatisfied(v,k)){
+            return true;
+        }
+        int left=cnt-1;
+        int right=n-1;
+        while(left>=0){
+            v[s[left]-'a']--;
+            v[s[right]-'a']++;
+            if(isSatisfied(v,k)){
+                return true;
+            }
+            left--;
+            right--;
+        }
+        return false;
+    }
+    public:
+    int takeCharacters(string s, int k) {
+        int ans=INT_MAX;
+        int n=s.length();
+        int low=0;
+        int high=n;
+        while(low<=high){
+            int mid=(low+high)/2;
+            if(isValid(s,k,mid)){
+                ans=min(ans,mid);
+                high=mid-1;
+            }
+            else{
+                low=mid+1;
+            }
+        }
+        if(ans==INT_MAX) return -1;
+        return ans;
+        
+    }
+    
 };
