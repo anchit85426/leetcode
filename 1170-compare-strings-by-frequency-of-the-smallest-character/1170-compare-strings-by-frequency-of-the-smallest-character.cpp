d5179aeabdb699aac87641d10152f8289bd7be01
anchit jain <75929617+anchit10@users.noreply.github.com>
2023-02-24 15:54:11 +0530

    Time: 18 ms (67.84%), Space: 11.6 MB (71.49%) - LeetHub

--- 1170-compare-strings-by-frequency-of-the-smallest-character/1170-compare-strings-by-frequency-of-the-smallest-character.cpp
+++ 1170-compare-strings-by-frequency-of-the-smallest-character/1170-compare-strings-by-frequency-of-the-smallest-character.cpp
@@ -5,17 +5,23 @@ public:
             return count(w.begin(),w.end(),*min_element(w.begin(),w.end()));
         };
         vector<int>count(13,0);
+        vector<int>res;
+        int n=words.size();
         for(auto it :words){
             count[f(it)]++;
+            res.push_back(f(it));
         }
-        for(int i=11;i>=0;i--){
-            count[i]+=count[i+1];
-        }
-        
+        sort(res.begin(),res.end());
         vector<int>ans;
         for(auto it :queries){
             int x=f(it);
-            ans.push_back(count[x+1]);
+            int x1=upper_bound(res.begin(),res.end(),x)-res.begin();
+            if(x1==n){
+                ans.push_back(0);
+                continue;
+            }
+            ans.push_back(n-x1);
+            
         }
         return ans;
     }
