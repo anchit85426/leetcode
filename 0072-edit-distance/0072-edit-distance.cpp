ad1b883609d4e260d6ad12b6721caa791ca54253
anchit jain <75929617+anchit10@users.noreply.github.com>
2023-03-15 13:03:58 +0530

    Time: 14 ms (61.96%), Space: 6.5 MB (94.34%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -13,18 +13,22 @@ public:
         int n=s1.size();
         int m=s2.size();
         
-        vector<vector<int>>dp(n+1,vector<int>(m+1,0));
+       // vector<vector<int>>dp(n+1,vector<int>(m+1,0));
         // return f(n-1,m-1,s1,s2,dp);
+        // for(int i=1;i<=n;i++){
+        //     dp[i][0]=i;
+        // }
+      
+        vector<int>prev(m+1,0),curr(m+1,0);
+      for(int j=0;j<=m;j++) prev[j]=j;  
         for(int i=1;i<=n;i++){
-            dp[i][0]=i;
-        }
-        for(int j=1;j<=m;j++) dp[0][j]=j;
-        for(int i=1;i<=n;i++){
+             curr[0]=i;
             for(int j=1;j<=m;j++){
-                if(s1[i-1]==s2[j-1]) dp[i][j]=dp[i-1][j-1];
-                else dp[i][j]=1+min(dp[i][j-1],min(dp[i-1][j-1],dp[i-1][j]));
+                if(s1[i-1]==s2[j-1]) curr[j]=prev[j-1];
+                else curr[j]=1+min(curr[j-1],min(prev[j-1],prev[j]));
             }
+            prev=curr;
         }
-        return dp[n][m];
+        return prev[m];
     }
 };
