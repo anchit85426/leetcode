ffed8881457cf72f9ef4b2a54c3ba820e2808b78
anchit jain <75929617+anchit85426@users.noreply.github.com>
2023-04-07 15:38:06 +0530

    Time: 82 ms (47.19%), Space: 30.9 MB (44.53%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.cpp
+++ 1020-number-of-enclaves/1020-number-of-enclaves.cpp
@@ -17,12 +17,12 @@ public:
           
              if(grid[i][0]==1 and vis[i][0]==0){
                 q.push({i,0});
-                vis[i][0]=1;
+               
             }
             
              if(grid[i][m-1]==1 and vis[i][m-1]==0){
                 q.push({i,m-1});
-                vis[i][m-1]=1;
+               
             }
             
             
@@ -31,12 +31,12 @@ public:
           
              if(grid[0][i]==1 and vis[0][i]==0){
                 q.push({0,i});
-                vis[0][i]=1;
+               
             }
             
              if(grid[n-1][i]==1 and vis[n-1][i]==0){
                 q.push({n-1,i});
-                vis[n-1][i]=1;
+               
             }
             
             
@@ -46,16 +46,22 @@ public:
             while(sq--){
                 int x=q.front().first;
                 int y=q.front().second;
-                count--;
+                
                 q.pop();
+                if(grid[x][y]==0){
+                    continue;
+                }
+                count--;
                 int dx[4]={0,-1,0,1};
                 int dy[4]={1,0,-1,0};
+                grid[x][y]=0;
                 for(int i=0;i<4;i++){
                     int newx=x+dx[i];
                     int newy=y+dy[i];
                     if(newx>=0 and newx<n and newy>=0 and newy<m and !vis[newx][newy] and grid[newx][newy]==1){
                         q.push({newx,newy});
-                        vis[newx][newy]=1;
+                        
+                        
                     }
                 }
             }
