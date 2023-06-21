58c70d459c8e0bffdaf0f2dc5beefe67525e0915
anchit jain <75929617+anchit85426@users.noreply.github.com>
2023-06-21 13:07:08 +0530

    Time: 229 ms (95.17%), Space: 129.8 MB (94.24%) - LeetHub

--- 2090-k-radius-subarray-averages/2090-k-radius-subarray-averages.cpp
+++ 2090-k-radius-subarray-averages/2090-k-radius-subarray-averages.cpp
@@ -2,25 +2,34 @@ class Solution {
 public:
     vector<int> getAverages(vector<int>& nums, int k) {
         int n=nums.size();
-        vector<long long int >prefix(n+1,0);
-        prefix[0]=nums[0];
-        for(int i=1;i<n;i++) prefix[i]=prefix[i-1]+nums[i];
-        vector<int>ans;
-        for(int i=0;i<n;i++){
-            int left=i-k;
-            int right=i+k;
-            if(left>=0 and right<n){
-                long long int sum=0;
-                sum+=prefix[right];
-                if(left>=1){
-                    sum-=prefix[left-1];
-                }
-                ans.push_back(sum/(right-left+1));
-            }
-            else{
-                ans.push_back(-1);
-            }
+        vector<int>ans(n,-1);
+       
+        long long int sum=0;
+        if(n<(2*k+1)){
+            return ans;
         }
+        for(int i=0;i<=2*k;i++){
+            sum+=nums[i];
+            
+        }
+        int j=2*k+1;
+        int f=0;
+        ans[k]=(sum/(2*k+1));
+        for(int i=k+1;i<n;i++){
+            if(j>=n){
+                return ans;
+            }
+            sum+=nums[j];
+            sum-=nums[f];
+            ans[i]=sum/(2*k+1);
+            f++;
+            j++;
+                               
+                               
+                              
+        }
+       
+       
         return ans;
     }
 };
