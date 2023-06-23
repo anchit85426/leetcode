4e7ebc9450f00a417c6cfd82af9a1054b69c17d0
anchit jain <75929617+anchit85426@users.noreply.github.com>
2023-06-23 15:50:55 +0530

    Time: 239 ms (17.66%), Space: 383.6 MB (5.42%) - LeetHub

---
+++ 0526-beautiful-arrangement/0526-beautiful-arrangement.cpp
@@ -0,0 +1,22 @@
+class Solution {
+    vector<vector<int>>dp;
+    int solve(int prevIndex,int mask,int n){
+        if(prevIndex==n+1) return 1;
+        if(dp[prevIndex][mask]!=-1) return dp[prevIndex][mask];
+        int total=0;
+        for(int j=1;j<=n;j++){
+            if(((mask>>j)&1)==0){
+            if(prevIndex==1 or prevIndex%j==0 or j%prevIndex==0){
+                total+=solve(prevIndex+1,mask|(1<<j),n);
+                
+            }
+         }
+        }
+        return dp[prevIndex][mask]=total;
+    }
+public:
+    int countArrangement(int n) {
+        dp.resize(20,vector<int>((1<<15+5),-1));
+        return solve(1,0,n);
+    }
+};
