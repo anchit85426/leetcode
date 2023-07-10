1f33d443735162cfc04d41bc8863157381aee65d
anchit jain <75929617+anchit85426@users.noreply.github.com>
2023-07-10 22:10:46 +0530

    Time: 272 ms (90.55%), Space: 144.6 MB (57.99%) - LeetHub

--- 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.cpp
+++ 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.cpp
@@ -22,7 +22,23 @@ class Solution {
     }
 public:
     int minDepth(TreeNode* root) {
-        if(root==NULL) return 0;
-        return Traversal(root);
+       queue<TreeNode*>q;
+        if(!root) return 0;
+        q.push(root);
+        int steps=0;
+        while(!q.empty()){
+            int size=q.size();
+            while(size--){
+                auto node=q.front();
+                q.pop();
+                if(node->right==NULL and node->left==NULL){
+                    return steps+1;
+                }
+                if(node->left) q.push(node->left);
+                if(node->right) q.push(node->right);
+            }
+            steps++;
+        }
+        return -1;
     }
 };
